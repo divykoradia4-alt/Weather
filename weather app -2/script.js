f26bbd2fc9ac67c04c774f9c98c462f5ad1c3faf
divykoradia4-alt <divykoradia4@gmail.com>
2025-12-25 22:21:17 +0530

    Add files via upload

---
+++ weather app -2/script.js
@@ -0,0 +1,79 @@
+const cityInput = document.querySelector('.city-input')
+const searchBtn = document.querySelector('.search-btn')
+
+const weatherInfoSection = document.querySelector('.weather-info')
+const notFoundSection = document.querySelector('.not-found')
+const searchCitySection = document.querySelector('.search-city')
+
+const countryTxt = document.querySelector('.country-txt')
+const temptxt = document.querySelector('.temp-txt')
+const conditionTxt = document.querySelector('.condition-txt')
+const humidityValueTxt = document.querySelector('.humidity-value-txt')
+const windValueTxt = document.querySelector('.wind-value-txt')
+const weathericon = document.querySelector('.weather-icon')
+const currentDateTxt = document.querySelector('.current-date-txt')
+const apikey = 'c591b5b311acfe5463ac7b6caa5760b7'
+
+searchBtn.addEventListener('click', () => {
+    if (cityInput.value.trim() !='') {
+    updateweatherInfo(cityInput.value)  
+    cityInput.value =''
+    cityInput.blur()
+    }
+})
+ cityInput.addEventListener('keydown',(event) => {
+    if (event.key == 'Enter' &&
+        cityInput.value.trim() !=''
+    ) {
+      updateweatherInfo(cityInput.value)   
+      cityInput.value =''
+      cityInput.blur()
+    }
+ })
+
+
+
+async function getFetchData(endPoint, city) {
+    const apiUrl = `https://api.openweathermap.org/data/2.5/${endPoint}?q=${city}&appid=${apikey}&units=metric`
+    const response = await fetch(apiUrl)
+    return response.json()
+}
+
+
+ async function updateweatherInfo(city) {
+    const weatherData = await getFetchData('weather', city)
+
+    if (weatherData.cod != 200) {
+        showDisplaySection(notFoundSection)
+        return
+    }
+
+   showDisplaySection(weatherInfoSection)
+
+    console.log(weatherData)
+
+    const {
+        name: country,
+        main: { temp, humidity },
+        weather: [{ id, main}],
+        wind: { speed }
+    } = weatherData
+
+    countryTxt.textContent = country
+    temptxt.textContent = Math.round(temp) + ' °C'
+    conditionTxt.textContent = main
+    humidityValueTxt.textContent = humidity + '%'
+    windValueTxt.textContent = speed + 'M/s'
+}
+     
+
+
+function showDisplaySection(section) {
+     [weatherInfoSection, searchCitySection, notFoundSection]
+     .forEach(section => section.style.display = 'none')
+
+     section.style.display = 'flex'
+}
+
+    
+ 
